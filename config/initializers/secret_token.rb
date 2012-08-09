470c822550378f93d0853cb30b7fa440832869db
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-08-08 19:29:23 -0700

    Add rails 3.2.7 and RVM setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Endpoint::Application.config.secret_token = '1748d4eec4f947f1d4c6293a3f26046799016f9754248967831d0c758320437200d1e267e28b20c7dd9208919f2022320e34159728f7a63e865491c2693f162b'
