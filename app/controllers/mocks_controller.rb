a20f6401334486e0304988b0df56da0fa9f812ff
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-08-08 21:10:49 -0700

    Print the request contents in the request listing.

--- app/controllers/mocks_controller.rb
+++ app/controllers/mocks_controller.rb
@@ -3,4 +3,8 @@ class MocksController < ApplicationController
     Request.create(:body => request.body.read, :fullpath => request.fullpath, :method => request.method)
     request.body.read
   end
+
+  def show
+    @request = Request.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Endpoint::Application.routes.draw do
+  match '/request/:id' => 'requests#show', :as => 'request'
   match '/requests' => 'requests#index'
   match '/*path' => 'mocks#action'
   match '/' => 'mocks#action'
