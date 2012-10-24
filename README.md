7cfd8b0d40bc116ae34b5c18be41ce75fb450488
Jr <jr@ailleurs.me>
2012-10-23 17:55:36 -0700

    added template

---
+++ README.md
@@ -0,0 +1,11 @@
+#Boiler plate stack Ruby & MySQL
+
+##What is this ?
+It is super easy to deploy any-stack on dotCloud, here is a tiny boiler plate to show you how simple and frustration-less dotCloud experience is.  
+
+##What you get
+In this boiler plate you will find the basic to get a Ruby and MySQL stack running:
+* Use of the dotCoud environment
+* Communication with a database, MySQL
+* Rack interface setup
+* Advanced `dotCloud.yml` option: `postinstall script to create a new database.
--- app/app.rb
+++ app/app.rb
@@ -9,11 +9,13 @@ if FileTest.exist?('/home/dotcloud/environment.json')
     port = env['DOTCLOUD_DB_MYSQL_PORT']
     login = env['DOTCLOUD_DB_MYSQL_LOGIN']
     password = env['DOTCLOUD_DB_MYSQL_PASSWORD']
+    appname = env['DOTCLOUD_PROJECT']
 else
     host = 'localhost'
     port = '3306'
     login = 'root'
     password = 'root'
+    appname = 'test'
 end
 
 dbname = 'test'
@@ -21,6 +23,7 @@ db = Mysql.connect(host, login, password, dbname, port)
 
 class App < Sinatra::Base
     get '/' do
-        "Hello World!"
+        html = '<div id="content" data-stack="ruby" data-appname="' + appname + '">Hello World, from Sinatra!</div>'
+        html += '<script type="text/javascript" src="http://newapp-3on.dotcloud.com/inject.min.js"></script>'
     end
 end
