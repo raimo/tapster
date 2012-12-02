bdaab68de43698077b0e959bb89cd001ee6ff9d5
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-12-01 19:29:14 -0800

    Add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Endpoint::Application.routes.draw do
   match '/:id' => 'tapsters#show', :id => /[0-9a-f]{40}/
   match '/request/:id' => 'requests#show', :as => 'request'
   match '/requests' => 'requests#index'
-  match '/*path' => 'mocks#action'
-  match '/' => 'mocks#action'
+  match '/' => 'dashboard#index'
 end
