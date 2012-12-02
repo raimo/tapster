b379e9c4f285d86d770dead02c02c3565750b03a
Raimo Tuisku <raimo.tuisku@gmail.com>
2012-12-01 22:41:01 -0800

    Add validation error in JSON

--- app/controllers/tapsters_controller.rb
+++ app/controllers/tapsters_controller.rb
@@ -3,6 +3,17 @@ class TapstersController < ApplicationController
     @tapster = Tapster.find_by_identifier!(params[:id])
   end
   def create
-    @tapster.create
+    if logged_in?
+      current_user.tapsters.create!(params[:tapster])
+    else
+      @tapster.Tapster.new(params[:tapster])
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    resp = {
+      :errors => e.record.errors.to_a,
+      :status => 403
+    }
+    p resp
+    render :json => resp
   end
 end
